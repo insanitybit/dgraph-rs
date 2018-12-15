5b71b6ca77a11035528c372da23beb5b86757151
Insanitybit <insanitybit@gmail.com>
2018-12-14 20:15:19 -0800

    Update Cargo.toml and gitignore

---
+++ src/lib.rs
@@ -0,0 +1,360 @@
+#[macro_use]
+extern crate failure;
+extern crate futures;
+extern crate futures_cpupool;
+extern crate grpc;
+extern crate httpbis;
+extern crate protobuf;
+extern crate serde;
+extern crate serde_json;
+
+
+use failure::Error;
+
+use crate::protos::{api, api_grpc::{self, Dgraph}};
+
+pub mod protos;
+
+pub struct Transaction {
+    context: api::TxnContext,
+    finished: bool,
+    read_only: bool,
+    mutated: bool,
+    client: api_grpc::DgraphClient,
+}
+
+impl Transaction {
+
+    pub fn query(&mut self, query: impl Into<String>) -> Result<api::Response, Error> {
+
+        if self.finished {
+            bail!("Transaction is completed");
+        }
+
+        let res = self.client.query(Default::default(),
+                               api::Request {
+                                   query: query.into(),
+                                   ..Default::default()
+                               }
+        ).wait()?;
+
+        let txn = match res.1.txn.as_ref() {
+            Some(txn) => txn,
+            None => bail!("Got empty transaction response back from query")
+        };
+
+        self.merge_context(txn)?;
+        Ok(res.1)
+    }
+
+    pub fn mutate(&mut self, mut mu: api::Mutation) -> Result<api::Assigned, Error> {
+
+        match (self.finished, self.read_only) {
+            (true, _) => bail!("Transaction is finished"),
+            (_, true) => bail!("Transaction is read only"),
+            _ => ()
+        }
+
+        self.mutated = true;
+        mu.start_ts = self.context.start_ts;
+        let commit_now = mu.commit_now;
+        let mu_res = self.client.mutate(
+            Default::default(),
+            mu
+        ).wait();
+
+        let mu_res = match mu_res {
+            Ok(mu_res) => mu_res,
+            Err(e) => {
+                let _ = self.discard();
+                bail!(e);
+            }
+        };
+
+        if commit_now {
+            self.finished = true;
+        }
+
+        let context = match mu_res.1.context.as_ref() {
+            Some(context) => context,
+            None => bail!("Missing transaction context on mutation response")
+        };
+
+        self.merge_context(context)?;
+        Ok(mu_res.1)
+    }
+
+    pub fn commit(mut self) -> Result<(), Error> {
+        match (self.finished, self.read_only) {
+            (true, _) => bail!("Transaction is finished"),
+            (_, true) => bail!("Transaction is read only"),
+            _ => ()
+        }
+
+        self.finished = true;
+
+        if !self.mutated {
+            return Ok(())
+        }
+
+
+        self.client.commit_or_abort(Default::default(), self.context.clone())
+            .wait()?;
+
+        Ok(())
+    }
+
+    fn discard(&mut self) -> Result<(), Error> {
+        if self.finished {
+            return Ok(())
+        }
+
+        self.finished = true;
+
+        if !self.mutated {
+            return Ok(())
+        }
+
+        self.context.aborted = true;
+
+        self.client.commit_or_abort(Default::default(), self.context.clone())
+            .wait()?;
+
+        Ok(())
+    }
+
+    fn merge_context(&mut self, src: &api::TxnContext) -> Result<(), Error> {
+        if self.context.start_ts == 0 {
+            self.context.start_ts = src.start_ts;
+        }
+
+        if self.context.start_ts != src.start_ts {
+            bail!("self.context.start_ts != src.start_ts")
+        }
+
+        for key in src.keys.iter() {
+            self.context.keys.push(key.clone());
+        }
+
+        for pred in src.preds.iter() {
+            self.context.preds.push(pred.clone());
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::Arc;
+
+    use grpc::{Client, ClientStub};
+    use grpc::ClientConf;
+    use std::collections::HashMap;
+
+    #[test]
+    fn insert_query() -> Result<(), Error> {
+        let addr = "localhost";
+        let port = 9080;
+
+        let client = api_grpc::DgraphClient::with_client(
+            Arc::new(
+                Client::new_plain(addr.as_ref(), port, ClientConf {
+                    ..Default::default()
+                })?
+            )
+        );
+
+        client.alter(
+            Default::default(),
+            api::Operation {
+                drop_all: true,
+                ..Default::default()
+            }
+        ).wait()?;
+
+        client.alter(
+            Default::default(),
+            api::Operation {
+                schema: "key: string @upsert @index(hash) .".into(),
+                ..Default::default()
+            }
+        ).wait()?;
+
+
+        client.mutate(
+            Default::default(),
+            api::Mutation {
+                set_json: r#"[{"key": "1234"}, {"key": "4567"}]"#.into(),
+                commit_now: true,
+                ..Default::default()
+            }
+        ).wait()?;
+
+
+        let query = r#"
+            {
+                q1(func: eq(key, "1234")) {
+                    uid,
+                },
+                q2(func: eq(key, "4567")) {
+                    uid,
+                }
+            }
+            "#;
+
+        let res = client.query(Default::default(),
+            api::Request {
+                query: query.into(),
+                ..Default::default()
+            }
+        ).wait()?;
+
+        let json_res: HashMap<String, serde_json::Value> = serde_json::from_slice(&res.1.json)?;
+
+        // Assert that we get uids back for both
+        &json_res["q1"][0]["uid"];
+        &json_res["q2"][0]["uid"];
+
+        // Assert that we get uids back for only those 2
+        assert!(&json_res.keys().len() == &2);
+
+        assert!(&json_res["q1"].as_array().unwrap().len() == &1);
+        assert!(&json_res["q2"].as_array().unwrap().len() == &1);
+
+        Ok(())
+    }
+
+
+    #[test]
+    fn insert_query_txn() -> Result<(), Error> {
+        let addr = "localhost";
+        let port = 9080;
+
+        let client = api_grpc::DgraphClient::with_client(
+            Arc::new(
+                Client::new_plain(addr.as_ref(), port, ClientConf {
+                    ..Default::default()
+                })?
+            )
+        );
+
+        client.alter(
+            Default::default(),
+            api::Operation {
+                drop_all: true,
+                ..Default::default()
+            }
+        ).wait()?;
+
+        client.alter(
+            Default::default(),
+            api::Operation {
+                schema: "key: string @upsert @index(hash) .".into(),
+                ..Default::default()
+            }
+        ).wait()?;
+
+
+
+        let mut handles = vec![];
+        for _ in 0..50 {
+            let client = api_grpc::DgraphClient::with_client(
+                Arc::new(
+                    Client::new_plain(addr.as_ref(), port, ClientConf {
+                        ..Default::default()
+                    })?
+                )
+            );
+
+            let mut tx = Transaction {
+                context: api::TxnContext::default(),
+                finished: false,
+                read_only: false,
+                mutated: false,
+                client,
+            };
+
+            let handle = std::thread::spawn(move || {
+                let query = r#"
+                    {
+                        q1(func: eq(key, "1234")) {
+                            uid,
+                        },
+                        q2(func: eq(key, "4567")) {
+                            uid,
+                        }
+                    }
+                    "#;
+
+                // hack for type inference
+                if false {
+                    bail!("impossible")
+                }
+
+                let res = tx.query(query)?;
+                let json_res: HashMap<String, serde_json::Value> = serde_json::from_slice(&res.json)?;
+
+                let uid1 = json_res.get("q1").and_then(|a| a.get(0)).and_then(|m| m.get("uid"));
+
+                let uid2 = json_res.get("q2").and_then(|a| a.get(0)).and_then(|m| m.get("uid"));
+
+                if let (Some(_), Some(_)) = (uid1, uid2) {
+                    return Ok(())
+                }
+
+                tx.mutate(
+                    api::Mutation {
+                        set_json: r#"[{"key": "1234"}, {"key": "4567"}]"#.into(),
+                        commit_now: false,
+                        ..Default::default()
+                    }
+                )?;
+
+                tx.commit()?;
+
+                Ok(())
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            let _ = handle.join();  // TODO: Ensure only transaction failures occurred
+        }
+
+        // Assert that they are created
+        let query = r#"
+            {
+                q1(func: eq(key, "1234")) {
+                    uid,
+                },
+                q2(func: eq(key, "4567")) {
+                    uid,
+                }
+            }
+            "#;
+
+        let res = client.query(Default::default(),
+                               api::Request {
+                                   query: query.into(),
+                                   ..Default::default()
+                               }
+        ).wait()?;
+
+        let json_res: HashMap<String, serde_json::Value> = serde_json::from_slice(&res.1.json)?;
+        println!("{:#?}", json_res);
+
+        // Assert that we get uids back for both
+        &json_res["q1"][0]["uid"];
+        &json_res["q2"][0]["uid"];
+
+        // Assert that we get uids back for only those 2
+        assert!(&json_res.keys().len() == &2);
+
+        assert!(&json_res["q1"].as_array().unwrap().len() == &1);
+        assert!(&json_res["q2"].as_array().unwrap().len() == &1);
+
+        Ok(())
+    }
+}
---
+++ src/protos/api_grpc.rs
@@ -0,0 +1,205 @@
+// This file is generated. Do not edit
+// @generated
+
+// https://github.com/Manishearth/rust-clippy/issues/702
+#![allow(unknown_lints)]
+#![allow(clippy)]
+
+#![cfg_attr(rustfmt, rustfmt_skip)]
+
+#![allow(box_pointers)]
+#![allow(dead_code)]
+#![allow(missing_docs)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(trivial_casts)]
+#![allow(unsafe_code)]
+#![allow(unused_imports)]
+#![allow(unused_results)]
+
+
+// interface
+
+pub trait Dgraph {
+    fn login(&self, o: ::grpc::RequestOptions, p: super::api::LoginRequest) -> ::grpc::SingleResponse<super::api::Response>;
+
+    fn query(&self, o: ::grpc::RequestOptions, p: super::api::Request) -> ::grpc::SingleResponse<super::api::Response>;
+
+    fn mutate(&self, o: ::grpc::RequestOptions, p: super::api::Mutation) -> ::grpc::SingleResponse<super::api::Assigned>;
+
+    fn alter(&self, o: ::grpc::RequestOptions, p: super::api::Operation) -> ::grpc::SingleResponse<super::api::Payload>;
+
+    fn commit_or_abort(&self, o: ::grpc::RequestOptions, p: super::api::TxnContext) -> ::grpc::SingleResponse<super::api::TxnContext>;
+
+    fn check_version(&self, o: ::grpc::RequestOptions, p: super::api::Check) -> ::grpc::SingleResponse<super::api::Version>;
+}
+
+// client
+
+pub struct DgraphClient {
+    grpc_client: ::std::sync::Arc<::grpc::Client>,
+    method_Login: ::std::sync::Arc<::grpc::rt::MethodDescriptor<super::api::LoginRequest, super::api::Response>>,
+    method_Query: ::std::sync::Arc<::grpc::rt::MethodDescriptor<super::api::Request, super::api::Response>>,
+    method_Mutate: ::std::sync::Arc<::grpc::rt::MethodDescriptor<super::api::Mutation, super::api::Assigned>>,
+    method_Alter: ::std::sync::Arc<::grpc::rt::MethodDescriptor<super::api::Operation, super::api::Payload>>,
+    method_CommitOrAbort: ::std::sync::Arc<::grpc::rt::MethodDescriptor<super::api::TxnContext, super::api::TxnContext>>,
+    method_CheckVersion: ::std::sync::Arc<::grpc::rt::MethodDescriptor<super::api::Check, super::api::Version>>,
+}
+
+impl ::grpc::ClientStub for DgraphClient {
+    fn with_client(grpc_client: ::std::sync::Arc<::grpc::Client>) -> Self {
+        DgraphClient {
+            grpc_client: grpc_client,
+            method_Login: ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                name: "/api.Dgraph/Login".to_string(),
+                streaming: ::grpc::rt::GrpcStreaming::Unary,
+                req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+            }),
+            method_Query: ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                name: "/api.Dgraph/Query".to_string(),
+                streaming: ::grpc::rt::GrpcStreaming::Unary,
+                req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+            }),
+            method_Mutate: ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                name: "/api.Dgraph/Mutate".to_string(),
+                streaming: ::grpc::rt::GrpcStreaming::Unary,
+                req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+            }),
+            method_Alter: ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                name: "/api.Dgraph/Alter".to_string(),
+                streaming: ::grpc::rt::GrpcStreaming::Unary,
+                req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+            }),
+            method_CommitOrAbort: ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                name: "/api.Dgraph/CommitOrAbort".to_string(),
+                streaming: ::grpc::rt::GrpcStreaming::Unary,
+                req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+            }),
+            method_CheckVersion: ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                name: "/api.Dgraph/CheckVersion".to_string(),
+                streaming: ::grpc::rt::GrpcStreaming::Unary,
+                req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+            }),
+        }
+    }
+}
+
+impl Dgraph for DgraphClient {
+    fn login(&self, o: ::grpc::RequestOptions, p: super::api::LoginRequest) -> ::grpc::SingleResponse<super::api::Response> {
+        self.grpc_client.call_unary(o, p, self.method_Login.clone())
+    }
+
+    fn query(&self, o: ::grpc::RequestOptions, p: super::api::Request) -> ::grpc::SingleResponse<super::api::Response> {
+        self.grpc_client.call_unary(o, p, self.method_Query.clone())
+    }
+
+    fn mutate(&self, o: ::grpc::RequestOptions, p: super::api::Mutation) -> ::grpc::SingleResponse<super::api::Assigned> {
+        self.grpc_client.call_unary(o, p, self.method_Mutate.clone())
+    }
+
+    fn alter(&self, o: ::grpc::RequestOptions, p: super::api::Operation) -> ::grpc::SingleResponse<super::api::Payload> {
+        self.grpc_client.call_unary(o, p, self.method_Alter.clone())
+    }
+
+    fn commit_or_abort(&self, o: ::grpc::RequestOptions, p: super::api::TxnContext) -> ::grpc::SingleResponse<super::api::TxnContext> {
+        self.grpc_client.call_unary(o, p, self.method_CommitOrAbort.clone())
+    }
+
+    fn check_version(&self, o: ::grpc::RequestOptions, p: super::api::Check) -> ::grpc::SingleResponse<super::api::Version> {
+        self.grpc_client.call_unary(o, p, self.method_CheckVersion.clone())
+    }
+}
+
+// server
+
+pub struct DgraphServer;
+
+
+impl DgraphServer {
+    pub fn new_service_def<H : Dgraph + 'static + Sync + Send + 'static>(handler: H) -> ::grpc::rt::ServerServiceDefinition {
+        let handler_arc = ::std::sync::Arc::new(handler);
+        ::grpc::rt::ServerServiceDefinition::new("/api.Dgraph",
+            vec![
+                ::grpc::rt::ServerMethod::new(
+                    ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                        name: "/api.Dgraph/Login".to_string(),
+                        streaming: ::grpc::rt::GrpcStreaming::Unary,
+                        req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                        resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                    }),
+                    {
+                        let handler_copy = handler_arc.clone();
+                        ::grpc::rt::MethodHandlerUnary::new(move |o, p| handler_copy.login(o, p))
+                    },
+                ),
+                ::grpc::rt::ServerMethod::new(
+                    ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                        name: "/api.Dgraph/Query".to_string(),
+                        streaming: ::grpc::rt::GrpcStreaming::Unary,
+                        req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                        resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                    }),
+                    {
+                        let handler_copy = handler_arc.clone();
+                        ::grpc::rt::MethodHandlerUnary::new(move |o, p| handler_copy.query(o, p))
+                    },
+                ),
+                ::grpc::rt::ServerMethod::new(
+                    ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                        name: "/api.Dgraph/Mutate".to_string(),
+                        streaming: ::grpc::rt::GrpcStreaming::Unary,
+                        req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                        resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                    }),
+                    {
+                        let handler_copy = handler_arc.clone();
+                        ::grpc::rt::MethodHandlerUnary::new(move |o, p| handler_copy.mutate(o, p))
+                    },
+                ),
+                ::grpc::rt::ServerMethod::new(
+                    ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                        name: "/api.Dgraph/Alter".to_string(),
+                        streaming: ::grpc::rt::GrpcStreaming::Unary,
+                        req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                        resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                    }),
+                    {
+                        let handler_copy = handler_arc.clone();
+                        ::grpc::rt::MethodHandlerUnary::new(move |o, p| handler_copy.alter(o, p))
+                    },
+                ),
+                ::grpc::rt::ServerMethod::new(
+                    ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                        name: "/api.Dgraph/CommitOrAbort".to_string(),
+                        streaming: ::grpc::rt::GrpcStreaming::Unary,
+                        req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                        resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                    }),
+                    {
+                        let handler_copy = handler_arc.clone();
+                        ::grpc::rt::MethodHandlerUnary::new(move |o, p| handler_copy.commit_or_abort(o, p))
+                    },
+                ),
+                ::grpc::rt::ServerMethod::new(
+                    ::std::sync::Arc::new(::grpc::rt::MethodDescriptor {
+                        name: "/api.Dgraph/CheckVersion".to_string(),
+                        streaming: ::grpc::rt::GrpcStreaming::Unary,
+                        req_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                        resp_marshaller: Box::new(::grpc::protobuf::MarshallerProtobuf),
+                    }),
+                    {
+                        let handler_copy = handler_arc.clone();
+                        ::grpc::rt::MethodHandlerUnary::new(move |o, p| handler_copy.check_version(o, p))
+                    },
+                ),
+            ],
+        )
+    }
+}
