53dceeced147d362472d7e1852889aa382a6120c
Insanitybit <insanitybit@gmail.com>
2019-11-14 23:55:55 -0800

    Exclude build.rs

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
 extern crate protoc_rust_grpc;
 
 fn main() {
-    protoc_rust_grpc::run(protoc_rust_grpc::Args {
-        out_dir: "src/protos/",
-        input: &["proto/api.proto"],
-        includes: &["proto"],
-        rust_protobuf: true, // also generate protobuf messages, not just services
-        ..Default::default()
-    }).expect("protoc");
+//    protoc_rust_grpc::run(protoc_rust_grpc::Args {
+//        out_dir: "src/protos/",
+//        input: &["proto/api.proto"],
+//        includes: &["proto"],
+//        rust_protobuf: true, // also generate protobuf messages, not just services
+ //       ..Default::default()
+ //   }).expect("protoc");
 }
